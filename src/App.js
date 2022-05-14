f58764c558b83da8c6ba84b821fa344b28c4ee52
stolomeo <tolomeosebastian@gmail.com>
2022-05-13 20:31:56 -0400

    add logic for buttons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
 import Keypad from "./Keypad";
+import { useState } from "react";
 
 function App() {
+  const [screenText, setScreenText] = useState("");
+
   const keypadButtons = [
     "1",
     "2",
@@ -16,12 +19,26 @@ function App() {
     "0",
     "√",
   ];
+  const handleClick = (e) => {
+    if (e.target.textContent === "<-") {
+      setScreenText((oldText) => oldText.slice(0, -1));
+    } else if (e.target.textContent === "√" && screenText !== "1024") {
+      console.log("access denidd");
+    } else if (e.target.textContent === "√" && screenText === "1024") {
+      console.log("Access Granted");
+    } else {
+      console.log("Enter Passcode");
+      setScreenText((oldText) => (oldText += e.target.textContent));
+    }
+  };
   const keypadElements = keypadButtons.map((btn) => {
-    return <Keypad value={btn} />;
+    return <Keypad value={btn} handleClick={handleClick} />;
   });
   return (
     <div className="keypad-container">
-      <div className="keypad-screen"></div>
+      <div className="keypad-screen">
+        <div className="keypad-text">{screenText}</div>
+      </div>
       <div className="keypad-grid">{keypadElements}</div>{" "}
     </div>
   );
