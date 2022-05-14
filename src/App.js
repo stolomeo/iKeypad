2948cedd26452700464766ed6891d4250b88e229
stolomeo <tolomeosebastian@gmail.com>
2022-05-13 20:22:35 -0400

    layout keypad container

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const keypadButtons = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "<-",
+    "0",
+    "√",
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="keypad-container">
+      <div className="keypad-screen"></div>
+      <div className="keypad-grid"></div>
     </div>
   );
 }
