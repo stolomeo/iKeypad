22ae53a2d2dee3fa1af4acfc1d8eefa66e801e07
stolomeo <tolomeosebastian@gmail.com>
2022-05-13 20:36:07 -0400

    replace btns with emojis

--- src/App.js
+++ src/App.js
@@ -5,44 +5,47 @@ import { useState } from "react";
 function App() {
   const [screenText, setScreenText] = useState("");
   const [isAccessGranted, setisAccessGranted] = useState("Enter Passcode");
-
   const keypadButtons = [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "<-",
-    "0",
-    "√",
+    "1️⃣",
+    "2️⃣",
+    "3️⃣",
+    "4️⃣",
+    "5️⃣",
+    "6️⃣",
+    "7️⃣",
+    "8️⃣",
+    "9️⃣",
+    "⬅️",
+    "0️⃣",
+    "✅",
   ];
   const handleClick = (e) => {
-    if (e.target.textContent === "<-") {
-      setScreenText((oldText) => oldText.slice(0, -1));
-    } else if (e.target.textContent === "√" && screenText !== "1024") {
+    if (e.target.textContent === "⬅️") {
+      setScreenText((oldText) => oldText.slice(0, -3));
+    } else if (e.target.textContent === "✅" && screenText !== "1️⃣0️⃣2️⃣4️⃣") {
       setisAccessGranted("Access Denied");
-    } else if (e.target.textContent === "√" && screenText === "1024") {
+    } else if (e.target.textContent === "✅" && screenText === "1️⃣0️⃣2️⃣4️⃣") {
       setisAccessGranted("Access Granted");
     } else {
       setisAccessGranted("Enter Passcode");
       setScreenText((oldText) => (oldText += e.target.textContent));
     }
   };
+
   const keypadElements = keypadButtons.map((btn) => {
     return <Keypad value={btn} handleClick={handleClick} />;
   });
+
   return (
-    <div className="keypad-container">
-      <h1 className="accessText">{isAccessGranted}</h1>
-      <div className="keypad-screen">
-        <div className="keypad-text">{screenText}</div>
+    <>
+      <div className="keypad-container">
+        <h1 className="accessText">{isAccessGranted}</h1>
+        <div className="keypad-screen">
+          <div className="keypad-text">{screenText}</div>
+        </div>
+        <div className="keypad-grid">{keypadElements}</div>
       </div>
-      <div className="keypad-grid">{keypadElements}</div>
-    </div>
+    </>
   );
 }
 
